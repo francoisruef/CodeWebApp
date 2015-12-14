deaf7c2ff7c2ac3930cbc11e4691cf8675dbb453
Francois Ruf <fruf@microsoft.com>
2015-12-14 17:54:21 +0100

    s

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var dispatcher = require('httpdispatcher');
+
+http.createServer(handleRequest).listen(process.env.PORT);
+
+function handleRequest(req, res) {
     console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
-}).listen(process.env.PORT);
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+    
+    dispatcher.dispatch(request, response);
+    
+}
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+function getDocs() {
+    // read lastDocID from database
+    
+    // get documents
+    
+    
+    // push new documents to event hub
+    
+    // save last docID
+
+}
