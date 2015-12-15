984fa70e8c676fe2be97e09f71464e8c63bc6296
Francois Ruf <fruf@microsoft.com>
2015-12-15 13:03:36 +0100

    a

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var http = require('http');
-var dispatcher = require('httpdispatcher');
+//var dispatcher = require('httpdispatcher');
 
 http.createServer(handleRequest).listen(process.env.PORT);
 
 function handleRequest(req, res) {
     console.log('Got request for ' + req.url);
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
     
-    dispatcher.dispatch(req, res);
+    //dispatcher.dispatch(req, res);
     
 }
 
+/*
 //For all your static (js/css/images/etc.) set the directory name (relative path).
 dispatcher.setStatic('resources');
 
@@ -38,3 +39,4 @@ function getDocs() {
     // save last docID
 
 }
+*/
